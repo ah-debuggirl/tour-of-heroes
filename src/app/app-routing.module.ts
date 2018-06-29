d53687dd71f9d8b337c7d97aabb4e98675ddc6bc
ahmarcela <srita.godinez.org@gmail.com>
2018-06-29 11:42:43 -0500

    Avance de tour of heroes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,8 +13,8 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  exports: [RouterModule];
-  imports: [RouterModule.forRoot(routes)];
+  exports: [RouterModule],
+  imports: [RouterModule.forRoot(routes)]
  
 })
 export class AppRoutingModule { }
